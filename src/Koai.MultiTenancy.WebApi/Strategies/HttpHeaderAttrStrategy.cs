af61939c7a48451b55745b3547e41a75286cac28
Bing Tran <bing.tranle@gmail.com>
2020-09-17 23:36:12 +0700

    Safe cast identity type

--- src/Koai.MultiTenancy.WebApi/Strategies/HttpHeaderAttrStrategy.cs
+++ src/Koai.MultiTenancy.WebApi/Strategies/HttpHeaderAttrStrategy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Koai.MultiTenancy.Abstractions;
 using Koai.MultiTenancy.Exceptions;
@@ -24,8 +25,14 @@ namespace Koai.MultiTenancy.WebApi.Strategies
             var hasTenant = httpContext.Request.Headers.TryGetValue(_tenantAttrKey, out StringValues tenantIdentifierStrVal);
             if (hasTenant && !StringValues.IsNullOrEmpty(tenantIdentifierStrVal))
             {
-                var tenantIdentifier = (TKey)Convert.ChangeType(tenantIdentifierStrVal.ToString(), typeof(TKey));
-                return Task.FromResult(tenantIdentifier);
+                var identityStrVal = tenantIdentifierStrVal.ToString();
+                var converter = TypeDescriptor.GetConverter(typeof(TKey));
+                if (converter.IsValid(identityStrVal))
+                {
+                    //var tenantIdentifier = (TKey)Convert.ChangeType(tenantIdentifierStrVal.ToString(), typeof(TKey));
+                    var tenantIdentifier = (TKey)converter.ConvertFrom(identityStrVal);
+                    return Task.FromResult(tenantIdentifier);
+                }
             }
 
             return Task.FromResult<TKey>(default);
